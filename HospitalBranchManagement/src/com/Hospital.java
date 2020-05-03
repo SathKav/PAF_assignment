4e3bd3f47a5c2f2c9bdeb5c84a860b519387c5af
SathKav <sathsaranika@gmail.com>
2020-05-03 22:23:30 +0530

    Create the connection class

--- HospitalBranchManagement/src/com/Hospital.java
+++ HospitalBranchManagement/src/com/Hospital.java
@@ -1,5 +1,22 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Hospital {
+	private Connection connect()
+	 {
+	 Connection con = null;
+	 try
+	 {
+	 Class.forName("com.mysql.jdbc.Driver");
+
+	 //Provide the correct details: DBServer/DBName, username, password
+	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_ca", "root", "8172429Amma*");
+	 }
+	 catch (Exception e)
+	 {e.printStackTrace();}
+	 return con;
+	 } 
 
 }
