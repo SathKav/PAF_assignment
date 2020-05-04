f29ff0b5b724ac7c8473301ce4d09972386bc469
SathKav <sathsaranika@gmail.com>
2020-05-04 14:12:07 +0530

    SAVE js function is implemented

--- HospitalBranchManagement/WebContent/Components/hospitals.js
+++ HospitalBranchManagement/WebContent/Components/hospitals.js
@@ -7,3 +7,41 @@ $(document).ready(function() {
 	
 	$("#alertError").hide();
 });
+
+//SAVE ============================================ 
+$(document).on("click", "#btnSave", function(event) {  
+	
+	// Clear status msges-------------  
+	$("#alertSuccess").text("");  
+	$("#alertSuccess").hide();  
+	$("#alertError").text("");  
+	$("#alertError").hide(); 
+
+	// Form validation----------------  
+	var status = validateItemForm(); 
+
+	// If not valid-------------------  
+	if (status != true)  {   
+		$("#alertError").text(status);   
+		$("#alertError").show();   
+		return;  
+	} 
+
+
+	// If valid-------------------------  
+	var type = ($("#hidhosIDSave").val() == "") ? "POST" : "PUT"; 
+	
+	$.ajax( {  
+		
+		url : "HospitalsAPI",  
+		type : type,  
+		data : $("#formHospital").serialize(),  
+		dataType : "text",  
+		complete : function(response, status)  {   
+			
+			onHospitalSaveComplete(response.responseText, status);  
+			
+		} 
+	}); 
+	
+}); 
