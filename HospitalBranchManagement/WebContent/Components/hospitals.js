0f355ff4f698047191a33cfcd604db78139f38f0
SathKav <sathsaranika@gmail.com>
2020-05-04 14:14:07 +0530

    onHospitalSaveComplete js function created

--- HospitalBranchManagement/WebContent/Components/hospitals.js
+++ HospitalBranchManagement/WebContent/Components/hospitals.js
@@ -45,3 +45,36 @@ $(document).on("click", "#btnSave", function(event) {
 	}); 
 	
 }); 
+
+function onHospitalSaveComplete(response, status) {  
+	
+	if (status == "success")  {   
+		
+		var resultSet = JSON.parse(response); 
+
+			if (resultSet.status.trim() == "success")   {    
+				
+				$("#alertSuccess").text("Successfully saved.");    
+				$("#alertSuccess").show(); 
+				$("#divItemsGrid").html(resultSet.data);   
+				
+			} else if (resultSet.status.trim() == "error")   {    
+				
+				$("#alertError").text(resultSet.data);    
+				$("#alertError").show();   
+				
+			} 
+
+	} else if (status == "error")  {   
+		
+		$("#alertError").text("Error while saving.");   
+		$("#alertError").show();  
+		
+	} else  {   
+		
+		$("#alertError").text("Unknown error while saving..");   
+		$("#alertError").show();  
+		
+	} 
+
+	$("#hidItemIDSave").val("");  $("#formItem")[0].reset(); } 
